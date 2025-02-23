5bdd162477ff3a21f902698cb3f74e8488a40ef8
Brit <brithd24@gmail.com>
2025-02-23 15:29:47 +0700

    Added Components and skills

---
+++ src/components/Skill.jsx
@@ -0,0 +1,6 @@
+const Skill = ({skill}) => {
+    return (
+        <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">{skill}</div>
+    )
+}
+export default Skill
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,14 @@
+import Skill from "../components/Skill"
+
 function About() {
+  const languages = ["Java", "Javascript", "TypeScript", "Python", "HTML/CSS", "C#", "SQL"]
+  const frameworks = ["React", "Flutter", "Flask", "Dash", "Tkinter", "Jupyter Notebook", "Tailwind", "Django", "Spring Boot", "React Native"]
+  const tools = ["Node", "Express", "Render", "AWS", "CircleCi", "Git", "SocketIO", "MySQL", "SQLite", "Render", "Google Firebase"]
+  const programs = ["iMovie", "Adobe Premiere Pro", "Adobe Photoshop", "Procreate", "Unity", "Twine", "Visual Studio Code", "Figma", "Inkscape", "Trello", "Jira", "TinkerCad", "Blender"]
+
 
   return (
-    <div className = 'lg:p-[150px]' id="about">
+    <div className='lg:p-[150px]' id="about">
       <p className="font-bold py-[70px] text-center text-6xl lg:text-8xl">About Me</p>
       <div className="flex">
         <div className="w-1/2 ml-[50px]">
@@ -9,45 +16,20 @@ function About() {
             <p className="font-bold text-4xl mb-[10px]">Skills</p>
             <p className="font-bold text-2xl">Programing Languages</p>
             <div className="flex flex-wrap mt-[10px]">
-              <div className="bg-gray-300 w-max rounded-xl m-1 ml-0 px-2 py-1">Java</div>
-              <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Javascript</div>
-              <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">TypeScript</div>
-              <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Python</div>
-              <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">HTML/CSS</div>
-              <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">C#</div>
+              {languages.map((skill) => <Skill key={skill} skill={skill} />)}
             </div>
-            <p className="font-bold mt-[15px] text-2xl">Tools and FrameWorks</p>
-              <div className="flex flex-wrap mt-[10px]">
-                <div className="bg-gray-300 w-max rounded-xl m-1 ml-0 px-2 py-1">ReactJs</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">SpringBoot</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">NodeJs</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Django</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Express</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Git</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Flask</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Dash</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Render</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Tkinter</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Jupyter Notebook</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Tailwind Css</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">SocketIO</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">SQLite</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">MySQL</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">AWS</div>
-              </div>
+            <p className="font-bold mt-[15px] text-2xl">FrameWorks</p>
+            <div className="flex flex-wrap mt-[10px]">
+              {frameworks.map((skill) => <Skill key={skill} skill={skill} />)}
+            </div>
+            <p className="font-bold mt-[15px] text-2xl">Tools</p>
+            <div className="flex flex-wrap mt-[10px]">
+              {tools.map((skill) => <Skill key={skill} skill={skill} />)}
+            </div>
             <p className="font-bold mt-[15px] text-2xl">Programs</p>
-              <div className="flex flex-wrap mt-[10px] ">
-                <div className="bg-gray-300 w-max rounded-xl m-1 ml-0 px-2 py-1">IMovie</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Adobe Premiere Pro</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Adobe Photoshop</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Procreate</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Unity</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Twine</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Itch.io</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Visual Studio Code</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Figma</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Inkscape</div>
-              </div>
+            <div className="flex flex-wrap mt-[10px] ">
+              {programs.map((skill) => <Skill key={skill} skill={skill} />)}
+            </div>
           </div>
         </div>
         <div className="">
@@ -60,10 +42,10 @@ function About() {
               <li>Collaborated with one other person to design the art and game mechanics</li>
               <li>Was the main designer for the art and game components</li>
             </ul>
-              <div className="flex flex-wrap items-center mt-[10px]">
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Adobe Photoshop</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Procreate</div>
-              </div>
+            <div className="flex flex-wrap items-center mt-[10px]">
+              <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Adobe Photoshop</div>
+              <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Procreate</div>
+            </div>
           </div>
           <div>
             <p className="font-bold text-2xl mt-[10px]">Java Backend Lead Internship - Shop Online New York</p>
@@ -73,12 +55,12 @@ function About() {
               <li>Made a backend database with MySQL using APIs</li>
               <li>Lead a team of developers to develop the backend on a timely manner</li>
             </ul>
-              <div className="flex flex-wrap items-center mt-[10px]">
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Java</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Springboot</div>
-                <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">MySQL</div>
-              </div>
-          </div> 
+            <div className="flex flex-wrap items-center mt-[10px]">
+              <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Java</div>
+              <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">Springboot</div>
+              <div className="bg-gray-300 w-max rounded-xl m-1 px-2 py-1">MySQL</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
